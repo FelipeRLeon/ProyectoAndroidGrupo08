f9bdb72399f99ab6d419dfdf81eacdc2da424e47
FelipeRLeon <feliiperestrepoleon@gmail.com>
2021-11-20 03:11:09 -0500

    Adicion pantalla de bienvenida: SplashScreen

--- app/src/main/java/com/bbt/ProyectoAndroidGrupo08/ProductoActivity.java
+++ app/src/main/java/com/bbt/ProyectoAndroidGrupo08/ProductoActivity.java
@@ -3,6 +3,7 @@ package com.bbt.ProyectoAndroidGrupo08;
 import android.content.Intent;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
